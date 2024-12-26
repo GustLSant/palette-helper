d8b5b70769752beea67253a7ea84c711f1e19196
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2024-12-25 21:30:10 -0300

    prep

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom'
-import { render, screen, fireEvent } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import App from '../App'
 
 jest.mock('../assets/react.svg', () => 'test-file-stub');
